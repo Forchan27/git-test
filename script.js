531aa891a21c609e8532416fd340bf8b8516c20c
Dmitry <dima.salov.55@gmail.com>
2023-06-22 23:36:49 +0300

    added function Math.min() in JavaScript

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
 
 const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"
 console.log(myStr)
+
+const array = [32, 115, -14, -2, 5, 13, 26, 88]
+function min(arr) {
+   return Math.max(...arr)
+}
+
+
+console.log(min(array))
